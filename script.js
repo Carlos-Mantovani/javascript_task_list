762d6c36db244c2f22c34989e9deb4a7bd9100ca
CarlosStrider120 <sherifinho.augusto@gmail.com>
2022-05-11 21:34:13 -0300

    created functions to mark and delete tasks

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 var userInput = document.getElementById('userinput');
 var button = document.getElementById('enter');
 var taskList = document.getElementById('task-list');
+var task = document.getElementsByClassName('task');
 
 function checkInputLength() {
     return (userInput.value.length > 0);
 }
 
+function markAsDone(e) {
+    e.target.classList.toggle('done');
+}
+
+function removeTask(e) {
+    e.target.parentElement.remove();
+}
+
 function createTask() {
     var task = document.createElement('li');
-        task.textContent = userInput.value;
-        task.classList.add('task');
-        taskList.appendChild(task);
-        userInput.value = '';
+    task.textContent = userInput.value;
+    task.classList.add('task');
+    task.addEventListener('click', markAsDone);
+    var delButton = document.createElement('button');
+    delButton.classList.add('del_btn');
+    delButton.textContent = 'delete';
+    delButton.addEventListener('click', removeTask);
+    taskList.appendChild(task);
+    task.appendChild(delButton);
+    userInput.value = '';
 }
 
 function addTaskAfterClick() {
@@ -29,6 +44,5 @@ function addTasAfterKeypress(e) {
 button.addEventListener('click', addTaskAfterClick);
 
 userInput.addEventListener('keypress', addTasAfterKeypress);
-
 
 
